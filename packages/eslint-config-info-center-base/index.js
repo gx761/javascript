58f986b554ab9b0197ad09c0686c1c794bf2951f
royguo(郭翔) <royguo@tencent.com>
2018-05-30 10:47:11 +0800

    fix style

--- packages/eslint-config-info-center-base/index.js
+++ packages/eslint-config-info-center-base/index.js
@@ -23,18 +23,18 @@ module.exports = {
   },
   rules: {
     strict: 'error',
-    "no-unused-expressions": ["error", { "allowShortCircuit": true }],
-    "no-console": [0],
-    "class-methods-use-this": [0],
-    "no-underscore-dangle": [0],
-    "max-len": [1, {"code": 140}],
-    "no-return-assign": [0],
-    "indent": [2, 2],
-    "no-mixed-operators": [0],
-    "radix": [0],
-    "no-use-before-define": [0],
-    "consistent-return": [0],
-    "no-plusplus": [0]
+    'no-unused-expressions': ['error', { allowShortCircuit: true }],
+    'no-console': [0],
+    'class-methods-use-this': [0],
+    'no-underscore-dangle': [0],
+    'max-len': [1, { code: 140 }],
+    'no-return-assign': [0],
+    indent: [2, 2],
+    'no-mixed-operators': [0],
+    radix: [0],
+    'no-use-before-define': [0],
+    'consistent-return': [0],
+    'no-plusplus': [0]
   },
   globals: {
     window: true,
@@ -52,7 +52,7 @@ module.exports = {
     before: true, // mocha before()
     after: true, // mocha after()
     Mock: true, // mockjs
-    Random: true,  // mockjs Mock.Random
+    Random: true, // mockjs Mock.Random
     localStorage: true,
     ga: true
   },
